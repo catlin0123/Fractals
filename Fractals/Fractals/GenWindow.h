009f1e8d180c69a5963e101eb0d3a8ecb483d497
catlin0123 <caitlin.taggart@mines.sdsmt.edu>
2014-10-26 19:37:15 -0600

    Commented code. Now all I need to do is test this stuff.

--- Fractals/Fractals/GenWindow.h
+++ Fractals/Fractals/GenWindow.h
@@ -1,13 +1,19 @@
+/*************************************************************************//**
+* @file
+*
+* @brief The header file for the GenWindow class which generates a fractal
+* draws that fractal on the screen. 
+****************************************************************************/
 #ifndef __GENWINDOW_H__
 #define __GENWINDOW_H__
 
+//includes
 #include <gl\freeglut.h>
 #include <gl\GLU.h>
 #include <vector>
-
 #include "Structs.h"
-
 using namespace std;
+
 
 class GenWindow
 {
@@ -17,28 +23,50 @@ public:
     static void Idle(); 
 	static void Display();
 	static void Reshape(int width, int height);
-	static void Mouse(int button, int state, int x, int y);
     static void GenerateFractal(vector<vector<double> > transforms, vector<Point>, double color[]);
 
 private:
-	GenWindow();
-	~GenWindow();
-
 	//Members
+    /*!
+    * @brief the window id to draw to
+    */
 	static int _windowID;
+
+    /*!
+    * @brief the newest point to be generated using ISF
+    */
     static ColorPoint idle;
+
+    /*!
+    * @brief tells whether to generate points or not
+    */
     static bool generate;
+
+    /*!
+    * @brief the color of the object points
+    */
     static double color[3];
+
+    /*!
+    * @brief the list of transforms to perform
+    */
     static vector<TransformMatrix> Transforms;
+
+    /*!
+    * @brief the points that make up the object drawn
+    */
     static vector<Point> points; 
+
+    /*!
+    * @brief a list of generated points 
+    */
     static vector<ColorPoint> genPoints; 
 
-
+    //Private Functions
     static void SolveEquations(vector<vector<double> > transforms);
     static ColorPoint TransformPoint(ColorPoint p, TransformMatrix m);
-
-	//Functions
+    GenWindow();
+    ~GenWindow();
 };
-
 #endif
 
